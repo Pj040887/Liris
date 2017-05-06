c9964327fb3e31ffdbcbff6f84e350467a2bcd2b
Patrick Jett <pj040887@gmail.com>
2017-05-06 02:27:29 -0400

    liri
    liri

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+
+  consumer_key: '8qv6WcKXlGWTx2ele4JynCl3q',
+  consumer_secret: '3JxfJs8ti8FR7tx4xidnE8WkDo82eqYuwNV21B2yYRHUCiu0bQ',
+  access_token_key: '857835777031634944-9YMkXrF3dSgt0VZwBlBM5COTPbR9z4z',
+  access_token_secret: 'HYK4S9FzCLF3gikCyXLAmqUzCYmJH8FiBdBIukhfcf9fi',
+
+}
